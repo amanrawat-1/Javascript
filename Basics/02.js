71339c4ba856767a11f810781d4d43930a68a7f9
Aman Rawat <140681334+amanrawat-1@users.noreply.github.com>
2024-02-29 04:58:00 +0000

    new file added

--- Basics/02.js
+++ Basics/02.js
@@ -1,2 +1,27 @@
-console.log(typeof undefined); // undefined
-console.log(typeof null); // object
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+// let a = false
+// console.log(typeof a)
+// let b = Number(a)
+// console.log(typeof b)
+// console.log(b)
+
+// let a = "23abc"
+// console.log(typeof a)
+// let b = Number(a)
+// console.log(typeof b)
+// console.log(b)
+
+
+// let a = 0
+// console.log(typeof a)
+// let b = Boolean(a)
+// console.log(typeof b)
+// console.log(b)
+
+
+// let a = 33
+// let b = String(a)
+// console.log(b)
+// console.log(typeof b)
